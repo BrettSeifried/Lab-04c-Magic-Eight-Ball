9edfbdd44c03d46490ece4f9343df39e3e3b37d0
Brett Seifried <seifriedbrett@gmail.com>
2021-08-26 16:39:19 -0700

    Created and Tested randomizer

---
+++ answers.js
@@ -0,0 +1,17 @@
+export const answers = [
+    'Push Ups',
+    'Burpess',
+    'Sit Ups',
+    'Flutter Kicks',
+    'Jumping Air Squats',
+    'Russian Twist',
+];
+
+export const reps = [
+    '5',
+    '8',
+    '10',
+    '12',
+    '15',
+    '20',
+];
--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 // import functions
-
+import { answers } from './answers.js';
+import { getRandomInt } from './randomNumber.js';
+import { reps } from './answers.js';
 // reference needed DOM elements
-
+const pushButton = document.getElementById('button'); 
+const warmUp = document.getElementById('warmup');
+const repsGo = document.getElementById('reps');
 
 // set event listeners 
+pushButton.addEventListener('click', () => {
+    const answer = answers[getRandomInt(5)];
+    warmUp.textContent = answer;
+});
+
+pushButton.addEventListener('click', () => {
+    const number = reps[getRandomInt(5)];
+    repsGo.textContent = number;
+});
+
+
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+    //const answer= answer[2]
+    //const randomIndex = getRandmNumber(0, answers, length) - 1
+    //cosnt randomAnswer = answers[randomIndex];
+    //answerDisplay.textConten = randsomAnswer;
